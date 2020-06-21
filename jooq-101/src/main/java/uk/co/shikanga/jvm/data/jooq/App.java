992ffd251862c70c9961011d11f0315ef6aaa35d
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-06-21 11:56:31 +0100

    working with jOOQ and transactions

--- jooq-101/src/main/java/uk/co/shikanga/jvm/data/jooq/App.java
+++ jooq-101/src/main/java/uk/co/shikanga/jvm/data/jooq/App.java
@@ -1,11 +1,9 @@
 package uk.co.shikanga.jvm.data.jooq;
 
 import org.jooq.DSLContext;
-import org.jooq.Record;
 import org.jooq.Record2;
 import org.jooq.Result;
 import org.jooq.impl.DSL;
-import uk.co.shikanga.jvm.data.jooq.public_.tables.Users;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -21,24 +19,32 @@ public class App {
                 "jdbc:h2:file:~/tools/mydb",
                 "admin",
                 "admin")) {
-            DSLContext dslContext = DSL.using(connection);
 
-            dslContext.insertInto(USERS, USERS.EMAIL).values("my@friend.com").execute();
+            DSL.using(connection).transaction(configuration -> {
 
-            dslContext.update(USERS)
-                    .set(USERS.EMAIL, "something@else.com")
-                    .where(USERS.EMAIL.startsWith("my@"))
-                    .execute();
+                DSLContext dslContext = DSL.using(configuration);
 
-            dslContext.deleteFrom(USERS)
-                    .where(USERS.EMAIL.eq("something@else.com"))
-                    .execute();
+                dslContext.insertInto(USERS, USERS.EMAIL).values("my@friend.com").execute();
 
-            Result<Record2<Integer, String>> records = dslContext
-                    .select(USERS.ID, USERS.EMAIL)
-                    .from(USERS)
-                    .fetch();
-            records.forEach(System.out::println);
+                dslContext.update(USERS)
+                        .set(USERS.EMAIL, "something@else.com")
+                        .where(USERS.EMAIL.startsWith("my@"))
+                        .execute();
+
+                dslContext.deleteFrom(USERS)
+                        .where(USERS.EMAIL.eq("something@else.com"))
+                        .execute();
+
+                Result<Record2<Integer, String>> records = dslContext
+                        .select(USERS.ID, USERS.EMAIL)
+                        .from(USERS)
+                        .fetch();
+                records.forEach(System.out::println);
+
+                //implicit commit
+
+            });
+
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
