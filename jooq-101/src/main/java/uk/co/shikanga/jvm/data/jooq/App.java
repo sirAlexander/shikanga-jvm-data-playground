b07612111035f488b14e9efe0fe10b2f98f3e5eb
sirAlexander <alexander.shikanga.tindi@gmail.com>
2020-06-21 10:18:29 +0100

    jOOQ with native select queries

--- jooq-101/src/main/java/uk/co/shikanga/jvm/data/jooq/App.java
+++ jooq-101/src/main/java/uk/co/shikanga/jvm/data/jooq/App.java
@@ -1,8 +1,37 @@
 package uk.co.shikanga.jvm.data.jooq;
 
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.impl.DSL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static uk.co.shikanga.jvm.data.jooq.public_.tables.Users.USERS;
+
 public class App {
 
     public static void main(String[] args) {
+
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:h2:file:~/tools/mydb",
+                "admin",
+                "admin")) {
+            DSLContext dslContext = DSL.using(connection);
+
+            Result<Record> records = dslContext.fetch("select id, email from users");
+            for (Record record : records) {
+                Integer id = record.get(USERS.ID);
+                String email = record.get(USERS.EMAIL);
+
+                System.out.println(record);
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        // DSLContext
 
     }
 }
